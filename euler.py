82908b205ae4632c9034fe9f6d3165ec371a1ea1
Raychell Valeria Arguedas Bolivar <valearguedas03@gmail.com>
2020-09-07 16:58:30 -0600

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,8 +1,26 @@
-def e_cuadratica(n):
-    # Implemente esta función
-    return 0
+#para menor espacio importar la biblioteca math 
+#y usar el .factorial
+def factorial (entero):
+    factorial = 1
+    while(entero > 0):
+        factorial= factorial*entero
+        entero   = entero - 1
+    return factorial 
 
+def e_cuadratica(n):
+    i = 0
+    e = 0
+    while(i<=n):
+        e += 1/factorial(i)
+        i += 1
+    return e
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+    i = 0
+    e = 0
+    factorial = 1
+    while(i<=n):
+        e += 1/factorial
+        i += 1
+        factorial = factorial*i
+    return e
